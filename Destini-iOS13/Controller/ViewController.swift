2d05edfb5c981adc231df029062ee510d75ac7f7
Marco Austria <maustria0507@gmail.com>
2022-09-30 13:28:59 -0700

    feat(complete): completed full challenge, working adventure game.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+        
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(sender.currentTitle!)
+       
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI(){
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
 
